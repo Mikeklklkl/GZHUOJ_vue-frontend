0160b77d02115b5b27a68fb13ddf9dd646d8ab21
陈旭明 <chenjiuririyue@outlook.com>
2024-08-03 20:21:24 +0800

    feat select markdown component
    选择了markdown组件

---
+++ src/components/MdEditor.vue
@@ -0,0 +1,68 @@
+<script setup>
+// 1.1 引入Vditor 构造函数
+import Vditor from 'vditor'
+// 1.2 引入样式
+import 'vditor/dist/index.css';
+import { ref, onMounted } from 'vue';
+ 
+// 2. 获取DOM引用
+const vditor = ref()
+ 
+// 3. 在组件初始化时，就创建Vditor对象，并引用
+onMounted(() => {
+  vditor.value = new Vditor('vditor',{
+    height: '80vh',
+    width: '50vw',
+    toolbar:[
+        "emoji",
+        "headings",
+        "bold",
+        "italic",
+        "strike",
+        "link",
+        "|",
+        "list",
+        "ordered-list",
+        "check",
+        "outdent",
+        "indent",
+        "|",
+        "quote",
+        "line",
+        "code",
+        "inline-code",
+        "insert-before",
+        "insert-after",
+        "|",
+        "upload",
+        "record",
+        "table",
+        "|",
+        "undo",
+        "redo",
+        "|",
+        "fullscreen",
+        "edit-mode",
+        {
+            name: "more",
+            toolbar: [
+                "both",
+                "code-theme",
+                "content-theme",
+                "export",
+                "outline",
+                "preview",
+                "info",
+                "help",
+            ],
+        },
+    ]
+  })
+})
+ 
+</script>
+ 
+<template>
+  <!-- 指定一个容器 -->
+  <div id="vditor"></div>
+</template>
